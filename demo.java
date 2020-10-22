49a84ad21c424c3b71c6c7aa779f69ba9b548678
popoolaa <aademolapopoola@gmail.com>
2020-10-21 21:46:32 -0400

    Fixing change

---
+++ demo.java
@@ -0,0 +1,26 @@
+total 459344
+lrwxrwxrwx   1 root root         7 Oct 20 11:42 bin -> usr/bin
+drwxr-xr-x   4 root root      4096 Oct 20 11:54 boot
+drwxrwxr-x   2 root root      4096 Oct 20 11:46 cdrom
+drwxr-xr-x  18 root root      4080 Oct 21 11:53 dev
+drwxr-xr-x 130 root root     12288 Oct 21 06:42 etc
+drwxr-xr-x   3 root root      4096 Oct 20 11:47 home
+lrwxrwxrwx   1 root root         7 Oct 20 11:42 lib -> usr/lib
+lrwxrwxrwx   1 root root         9 Oct 20 11:42 lib32 -> usr/lib32
+lrwxrwxrwx   1 root root         9 Oct 20 11:42 lib64 -> usr/lib64
+lrwxrwxrwx   1 root root        10 Oct 20 11:42 libx32 -> usr/libx32
+drwx------   2 root root     16384 Oct 20 11:41 lost+found
+drwxr-xr-x   2 root root      4096 Jul 31 12:27 media
+drwxr-xr-x   2 root root      4096 Jul 31 12:27 mnt
+drwxr-xr-x   2 root root      4096 Jul 31 12:27 opt
+dr-xr-xr-x 180 root root         0 Oct 21 11:53 proc
+drwx------   4 root root      4096 Oct 21 21:34 root
+drwxr-xr-x  31 root root       820 Oct 21 14:48 run
+lrwxrwxrwx   1 root root         8 Oct 20 11:42 sbin -> usr/sbin
+drwxr-xr-x   8 root root      4096 Oct 20 12:00 snap
+drwxr-xr-x   2 root root      4096 Jul 31 12:27 srv
+-rw-------   1 root root 470287360 Oct 20 11:42 swapfile
+dr-xr-xr-x  13 root root         0 Oct 21 11:53 sys
+drwxrwxrwt  18 root root      4096 Oct 21 21:30 tmp
+drwxr-xr-x  14 root root      4096 Jul 31 12:28 usr
+drwxr-xr-x  14 root root      4096 Jul 31 12:35 var
